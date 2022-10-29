cadc978a5bb324587668dda40d18fcc324011fbc
Binh <binhngswh01067@fpt.edu.vn>
2022-10-29 17:30:26 +0700

    update

--- test_and_revision_1/test3.rb
+++ test_and_revision_1/test3.rb
@@ -3,57 +3,57 @@ require './input_functions'
 
 # Complete the code below
 # Use input_functions to read the data from the user
-class Car 
-    attr_accessor :id, :manufacturer, :model, :kilometres
-    def initialize (id, manufacturer, model, kilometres)
+class Race 
+    attr_accessor :description, :id, :time, :location
+    def initialize (description, id, time, location)
+        @description = description
         @id = id
-        @manufacturer = manufacturer
-        @model = model
-        @kilometres = kilometres
+        @time = time
+        @location = location
     end
 end
 
-def read_car()
-	car_id = read_integer("Enter car id:")
-    car_manufacturer = read_string("Enter manufacturer:")
-    car_model = read_string("Enter model:")
-    car_kilometres = read_string("Enter kilometres:")
-    car = Car.new(car_id, car_manufacturer, car_model, car_kilometres)
-    return car
+def read_race
+	description = read_integer("Enter race description:")
+    id = read_string("Enter id:")
+    time = read_string("Enter time:")
+    location = read_string("Enter location:")
+    race = Race.new(description, id, time, location)
+    return race
 end
 
 
-def read_cars()
-	count = read_integer("How many cars are you entering:")
-    cars = Array.new
+def read_races
+	count = read_integer("How many races are you entering:")
+    races = Array.new
     index =0
-    while index<count
-     car = read_car()
-     cars << car
-     index+=1
+    while (index < count)
+     race = read_race()
+     races << race
+     index += 1
     end
-    return cars
+    return races
 end
 
-def print_car(car)
-	puts ('Id: ' + car.id.to_s)
-    puts ('Manufacturer: '+ car.manufacturer.to_s)
-    puts('Model: ' + car.model.to_s)
-    puts('Kilometres: ' + car.kilometres.to_s)
+def print_race(race)
+	puts ('Race Description ' + race.description.to_s)
+    puts ('Id '+ race.id.to_s)
+    puts('Time ' + race.time.to_s)
+    puts('Location ' + race.location.to_s)
 end
 
-def print_cars(cars)
-	count = cars.length
+def print_races(races)
+	count = races.length
     index = 0
-    while index < count do
-        print_car(cars[index])
-        index +=1
+    while (index < count)
+        print_race(races[index])
+        index += 1
     end   
 end
 
-def main()
-	cars = read_cars()
-	print_cars(cars)
+def main
+	races = read_races()
+	print_races(races)
 end
 
-main()
+main
